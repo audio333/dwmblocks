bfa0bf243c7353e119b2e1cb2d288973804097a5
jefbalmes <jefbalmes@gmail.com>
2024-11-08 11:06:46 +0800

    added blocks - mpv & ytm

--- config.h
+++ config.h
@@ -13,23 +13,25 @@ static const Block blocks[] = {
 	/*{"",	"sb-torrent",	20,	7}, */
 	/* {"",	"sb-memory",	10,	14}, */
 	{"",	"sb-internet",	5,	4}, 
-	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3}, 
 	{"",	"sb-cpu",		10,	18}, 
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	/* {"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-uptime",	60,	1}, 
 	{"",	"sb-clock",	60,	1}, 
+	{"",	"sb-volume",	0,	10},
+	/* {"",	"sb-ytm",	5,	14}, */
 	{"",	"sb-cmus",	0,	11},
+	{"",	"sb-mpv",	5,	12},
 	{"",	"sb-tsp",	10,	26},
-	{"",	"sb-tsp-dl",	10,	26},
-	{"",	"sb-tsp-md",	5,	26},
+	{"",	"sb-tsp-dl",	10,	25},
+	/* {"",	"sb-tsp-md",	5,	24}, */
 	/* {"",	"sb-iplocate", 0,	27}, */
 	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
