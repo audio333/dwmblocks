390bbb6b9024663762467db5c578845593bfbc8f
jefbalmes <jefbalmes@gmail.com>
2024-11-16 12:03:58 +0800

    added module for newsboat, unread feeds

--- config.h
+++ config.h
@@ -5,7 +5,6 @@ static const Block blocks[] = {
 	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
 	/* {"",	"sb-music",	0,	11}, */
 	/* {"",	"sb-pacpackages",	0,	8}, */
-	/* {"",	"sb-news",		0,	6}, */
 	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
 	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
@@ -13,6 +12,7 @@ static const Block blocks[] = {
 	/*{"",	"sb-torrent",	20,	7}, */
 	/* {"",	"sb-memory",	10,	14}, */
 	{"",	"sb-internet",	5,	4}, 
+	{"",	"sb-news",		0,	6}, 
 	{"",	"sb-battery",	5,	3}, 
 	{"",	"sb-cpu",		10,	18}, 
 	/* {"",	"sb-moonphase",	18000,	17}, */
@@ -24,7 +24,7 @@ static const Block blocks[] = {
 	{"",	"sb-cmus",	0,	11},
 	{"",	"sb-mpv",	5,	12},
 	{"",	"sb-tsp",	10,	26},
-	{"",	"sb-tsp-dl",	10,	25},
+	{"",	"sb-tsp-dl",	15,	25},
 	/* {"",	"sb-tsp-md",	5,	24}, */
 	/* {"",	"sb-iplocate", 0,	27}, */
 	/* {"",	"sb-help-icon",	0,	15}, */
